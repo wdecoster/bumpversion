cc90183a48b6272e7e3cda5f2e942befd0d459a0
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-06-21 19:17:41 +0200

    good to go

--- bumpversion/bumpversion.py
+++ bumpversion/bumpversion.py
@@ -1,4 +1,10 @@
 import os
 
-
 versionfile = [os.path.join(root, x) for root, dirs, files in os.walk(os.getcwd()) for x in files if x == "version.py"]
+
+if len(versionfile) == 1:
+    with open(versionfile[0]) as versionf:
+        version = versionf.read().split("\"")[1]
+    os.system("git commit -m 'bumping version to {}'".format(version))
+else:
+    print("Found multiple occurences of version.py - doing nothing.")
