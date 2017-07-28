e2540bd1f29565a1e89e89d2457b013caaf46ef4
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-07-28 19:57:56 +0200

    wrote in main() function

--- bumpversion/bumpversion.py
+++ bumpversion/bumpversion.py
@@ -2,11 +2,15 @@
 import os
 import sys
 
-versionfile = [os.path.join(root, x) for root, dirs, files in os.walk(os.getcwd()) for x in files if x == "version.py"]
+def main()
+    versionfile = [os.path.join(root, x) for root, dirs, files in os.walk(os.getcwd()) for x in files if x == "version.py"]
+    if len(versionfile) == 1:
+        with open(versionfile[0]) as versionf:
+            version = versionf.read().split("\"")[1]
+        os.system("git commit -m 'bumping version to {}' {}".format(version, versionfile[0]))
+    else:
+        sys.exit("Found multiple occurences of version.py - doing nothing.")
 
-if len(versionfile) == 1:
-    with open(versionfile[0]) as versionf:
-        version = versionf.read().split("\"")[1]
-    os.system("git commit -m 'bumping version to {}' {}".format(version, versionfile[0]))
-else:
-    sys.exit("Found multiple occurences of version.py - doing nothing.")
+
+if __name__ == '__main__':
+    main()
